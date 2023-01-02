ceba7f38b16d6ebdbf3458095a309be38ad1ddf7
Ryan Chiang <wsad71155@gmail.com>
2023-01-02 17:28:46 +0800

    Update 2023-01-02T09:28:44.398Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,7 +3,4 @@
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
   target: '#navbar'
 });
-var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-  target: '#navbar-mobile'
-});
 //# sourceMappingURL=all.js.map
